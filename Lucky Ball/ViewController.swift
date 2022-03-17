75f94b7a81a8f6b16eb5019a89c66c1d58b51556
Francielly Kelly <fransilva.fks@gmail.com>
2022-03-17 20:28:28 -0300

    feat: answer user questions

--- Lucky Ball/ViewController.swift
+++ Lucky Ball/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var spaceImageView: UIImageView!
+    @IBOutlet weak var answerImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
     }
-
-
+    
+    @IBAction func askButtonPressed(_ sender: UIButton) {
+        let ballArray = [
+            UIImage(named:"ball1" )!,
+            UIImage(named:"ball2" )!,
+            UIImage(named:"ball3" )!,
+            UIImage(named:"ball4" )!,
+            UIImage(named:"ball5" )!
+        ]
+        
+        answerImageView.image = ballArray [Int.random(in: 0...4)]
+    }
 }
-
